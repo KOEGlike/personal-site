99c7d00222bf561f404ab3dd76148d6ad9684e08
KOEGlike <koeglike@proton.me>
2026-09-07 16:28:23 +0300

    moved to svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+ Hi there, I'm Marcell!
+ <br />
+ I'm excited about embedded systems and designing PCBs!
+ <br />
+ <br />
+ Here are some of my projects:
+ <ul>
+     <li>
+         <b> <a href="https://github.com/koeglike/meko" target="_blank" rel="noopener noreferrer"> Meko </a> </b
+         >: An E-Paper audio player. Currently working on V3, for which I'm making a custom SBC. Fully journaled.
+     </li>
+     <li>
+         <b>
+             <a href="https://github.com/hackclub/hackxpansion" target="_blank" rel="noopener noreferrer">
+                 Hackxpansion
+             </a> </b
+         >: A modular game console based on the RP2354, which I made for my Hack Club internship
+     </li>
+     <li>
+         <b> <a href="https://github.com/koeglike/mito" target="_blank" rel="noopener noreferrer"> Mito </a> </b
+         >: A wireless hot-swap low-profile split keyboard with RGB running on ZMK
+     </li>
+ </ul>
+ <br />
+ Check out my <a href="https://github.com/koeglike" target="_blank" rel="noopener noreferrer">GitHub</a> and
+ <a href="https://www.linkedin.com/in/marcell-balazs123/" target="_blank" rel="noopener noreferrer">LinkedIn.</a>
+ <br />
+ My email is: <a href="mailto:koeglike@proton.me">koeglike@proton.me</a>
