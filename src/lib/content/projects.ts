0cad58ed405e0c9589ace5f92384626d5841b77d
KOEGlike <koeglike@proton.me>
2026-09-08 00:12:45 +0300

    fixed hackxpansion description

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -3,12 +3,13 @@ export const projects = [
 		name: 'Meko',
 		repo: 'https://github.com/koeglike/meko',
 		description:
-			"An E-Paper audio player. Currently working on V3, for which I'm making a custom SBC. Fully journaled."
+			"An E-Paper audio player. Currently working on V3, for which I'm making a custom SBC. Fully journaled"
 	},
 	{
 		name: 'Hackxpansion',
 		repo: 'https://github.com/hackclub/hackxpansion',
-		description: 'https://github.com/hackclub/hackxpansion'
+		description:
+			'A modular game console based on the RP2354, which I made for my Hack Club internship'
 	},
 	{
 		name: 'Mito',
